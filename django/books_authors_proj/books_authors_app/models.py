64f04390c536a17876e7678f14f6c61cfc51471f
beenjammin <btjensen@mtu.edu>
2020-05-13 17:18:06 -0700

    Books Authors (Shell)

---
+++ django/books_authors_proj/books_authors_app/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+
+class books(models.Model):
+    title = models.CharField(max_length=255)
+    desc = models.TextField()
+    # authors_included: list of authors for this book
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+class authors(models.Model):
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    notes = models.TextField()
+    authored_books = models.ManyToManyField(books,related_name="authors_included")
+    # books_authored: list of books by this author
---
+++ django/books_authors_proj/books_authors_app/views.py
@@ -0,0 +1,5 @@
+from django.shortcuts import render, HttpResponse
+from .models import *
+
+def index(request):
+    return HttpResponse('THis Works!!')
